6198d928bcac45bbd7d79b855f20b6c67341aeff
Georgi Ivanov <3618084+georgiIvanov@users.noreply.github.com>
2024-03-22 15:54:45 +0200

    Deploy huff contract for test, write value test

--- test/AssemblyFVCourse/horse-storeV1/BaseTestV1.t.sol
+++ test/AssemblyFVCourse/horse-storeV1/BaseTestV1.t.sol
@@ -6,6 +6,9 @@ import "@horse-storeV1/HorseStore.sol";
 
 // Contract containing actual tests
 // Child contracts instantiate solc or huff version of the contract
+// forge test --mc HorseStore -- runs both solc and huff tests
+// forge test --mc HorseStoreHuff -- runs only huff
+// forge test --mc HorseStoreHuff --debug testWriteValue -- runs in debug mode
 abstract contract BaseTestV1 is Test {
   
   HorseStore public horseStore;
@@ -16,5 +19,10 @@ abstract contract BaseTestV1 is Test {
   function testReadValue() public {
     uint256 value = horseStore.readNumberOfHorses();
     assertEq(value, 0);
+  }
+
+  function testWriteValue(uint256 numberOfHorses) public {
+    horseStore.updateHorseNumber(numberOfHorses);
+    assertEq(horseStore.readNumberOfHorses(), numberOfHorses);
   }
 }
--- test/AssemblyFVCourse/horse-storeV1/HorseStoreHuff.t.sol
+++ test/AssemblyFVCourse/horse-storeV1/HorseStoreHuff.t.sol
@@ -3,9 +3,13 @@ pragma solidity ^0.8.23;
 
 import "./BaseTestV1.t.sol";
 import "@horse-storeV1/HorseStore.sol";
+import "@foundry-huff/HuffDeployer.sol";
 
-contract HorseStoreSolc is BaseTestV1 {
+contract HorseStoreHuff is BaseTestV1 {
+  // no need to specify src prefix or .huff extension
+  string public constant HORSE_STORE_LOCATION = "AssemblyFVCourse/horse-storeV1/HorseStore";
+
   function setUp() public override {
-    
+    horseStore = HorseStore(HuffDeployer.config().deploy(HORSE_STORE_LOCATION));
   }
 }
