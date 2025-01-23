99d693b7e3d2b4d164600902d10d10f0a7d93ca1
Georgi Ivanov <3618084+georgiIvanov@users.noreply.github.com>
2025-01-23 08:59:57 +0200

    casting uints

--- test/OperatorPriority.t.sol
+++ test/OperatorPriority.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.28;
 
 import {Test, console2, console} from "forge-std/Test.sol";
+import {sl} from "@solc-log/sl.sol";
 
 /// Test contract to verify the priority of operators
 /// Inspiration - https://github.com/x676f64/secureum-mind_map/blob/master/quizzes/2.%20Solidity%20101.md#q24-if-a--1-then-which-of-the-following-isare-true
@@ -22,5 +23,26 @@ contract OperatorPriorityTest is Test {
     b = ++a;
     assertEq(b, 2);
     assertEq(a, 2);
+  }
+
+  /// Test behavior of casting 
+  /// https://github.com/x676f64/secureum-mind_map/blob/master/quizzes/2.%20Solidity%20101.md#q26-conversions-in-solidity-have-the-following-behavior
+  function testCastUint() public {
+    // val = 1001111111110001
+    uint16 val = 40945;
+    assertEq(val, 40945);
+    sl.logAsBin("uint16.max: ", val);
+
+    // needs explicit conversion
+    // notice which bits were removed - higher order!
+    uint8 smaller = uint8(val);
+    
+    sl.logAsBin("uint8.max: ", smaller);
+    assertEq(smaller, 241);
+
+    // implicit upcasting
+    // adds bits on the left; (padding in higher order)
+    uint16 bigger = smaller;
+    sl.logAsBin("implicit upcast to uint16: ", bigger);
   }
 }
